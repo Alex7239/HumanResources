8c8c7165e6307c2eace0185e53b47359e2e2af06
Juan Carlos Romero <Juan Carlos Romero@DESKTOP-T26H7P1>
2021-02-04 21:57:42 -0600

    Version 1.1

---
+++ src/mx/edu/cesba/model/Employee.java
@@ -0,0 +1,24 @@
+/*
+Nombre: Valenzuela Romero Manuel Alejandro
+Matricula:2022742
+ */
+package mx.edu.cesba.model;
+
+
+public class Employee {
+    public int id;
+    public String firstName;
+    public String lastName;
+    public int age;
+    public int phoneNumber;
+    public double salary;
+    public String hiredate;
+    
+   public void getDetails() { //METODO
+        
+       
+       
+}
+
+    
+}
