16a121a215773315e7f2c9850ac49994c7a503f2
Juan Carlos Romero <Juan Carlos Romero@DESKTOP-T26H7P1>
2021-02-04 22:09:26 -0600

    Version 1.1

---
+++ src/mx/edu/cesba/model/Department.java
@@ -0,0 +1,19 @@
+/*
+ Nombre: Valenzuela Romero Manuel Alejandro
+ Matricula:2022742
+ */
+package mx.edu.cesba.model;
+
+
+public class Department {
+    public int id;
+    public String title ;
+    public String location;
+    
+    
+    public void getDetails() { //METODO
+       
+       
+       
+}
+}
---
+++ src/mx/edu/cesba/model/Job.java
@@ -0,0 +1,21 @@
+/*
+ Nombre: Valenzuela Romero Manuel Alejandro
+ Matricula:2022742
+ */
+package mx.edu.cesba.model;
+
+
+public class Job {
+    
+    public int id;
+    public String title ;
+    public double minSalary;
+    public double maxSalary;
+    
+    public void getDetails() { //METODO
+       
+       
+       
+}
+    
+}
